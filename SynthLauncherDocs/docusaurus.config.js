2228d16983248e88601a4e7628134b157a4d2e03
Methuja Ganewattha <amethujarandira552@gmail.com>
2025-06-14 21:58:23 +1200

    a

--- SynthLauncherDocs/docusaurus.config.js
+++ SynthLauncherDocs/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  url: 'https://synthlauncher.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/SynthLauncherDocs',
+  baseUrl: '/SynthLauncherDocs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -140,7 +140,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/SynthLauncher/SynthLauncherDocs',
               },
             ],
           },
